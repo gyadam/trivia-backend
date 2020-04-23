39c2422c4245fb9ac88e70ee221e46e16047fcc2
adam-ubuntu <gy.adam90@gmail.com>
2020-04-23 15:54:11 -0700

    Setup endpoint tests on Heroku app

--- test_flaskr.py
+++ test_flaskr.py
@@ -4,7 +4,7 @@ import json
 from flask_sqlalchemy import SQLAlchemy
 from flask import jsonify
 
-from flaskr import create_app
+import app
 from models import setup_db, Question, Category
 
 
@@ -13,11 +13,9 @@ class TriviaTestCase(unittest.TestCase):
 
     def setUp(self):
         """Define test variables and initialize app."""
-        self.app = create_app()
+        self.app = app.create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgresql://{}/{}".format(
-            'postgres:@localhost:5432', self.database_name)
+        self.database_path = os.getenv("DATABASE_URL")
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -28,8 +26,10 @@ class TriviaTestCase(unittest.TestCase):
             self.db.create_all()
 
     def tearDown(self):
-        """Executed after reach test"""
-        pass
+        # Remove any test questions added by test cases
+        test_entries = Question.query.filter_by(question='Test question')
+        for test_entry in test_entries:
+            self.client().delete('/questions/' + str(test_entry.id))
 
     def test_get_paginated_questions(self):
         res = self.client().get('/questions')
@@ -37,7 +37,6 @@ class TriviaTestCase(unittest.TestCase):
 
         self.assertEqual(res.status_code, 200)
         self.assertTrue(data['total_questions'])
-        self.assertTrue(data['current_category'])
         self.assertTrue(data['categories'])
 
     def test_get_categories(self):
@@ -47,25 +46,17 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['categories']['1'], 'Science')
 
-    def test_delete_question(self):
-        num_questions_before = Question.query.count()
-        first_entry_id = Question.query.first().id
-        res = self.client().delete('/questions/' + str(first_entry_id))
-        num_questions_after = Question.query.count()
-
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(num_questions_before - 1, num_questions_after)
-
     def test_post_question(self):
         add_res = self.client().get('/add')
         self.assertEqual(add_res.status_code, 200)
 
         num_questions_before = Question.query.count()
+        # Add test question, which is deleted by the teardown() function
         mock_data = json.dumps({
-            'question': 'What day is it today?',
-            'answer': 'Tuesday',
-            'category': '1',
-            'difficulty': '1'
+            'question': 'Test question',
+            'answer': 'Test answer',
+            'category': 1,
+            'difficulty': 1
         })
         post_res = self.client().post('/questions', data=mock_data,
                                       content_type='application/json')
@@ -75,16 +66,18 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(num_questions_before + 1, num_questions_after)
 
     def test_search(self):
+        # Add test question to search for
         test_question = Question(
             question='Test question', answer='Test answer', category=1, difficulty=1)
         test_question.insert()
+
+        # Add search term
         mock_data = json.dumps({
             'searchTerm': 'Test'
         })
         res = self.client().post('/questions', data=mock_data,
                                  content_type='application/json')
         data = json.loads(res.data)
-
         self.assertEqual(res.status_code, 200)
         self.assertTrue(data['questions'])
 
@@ -100,4 +93,5 @@ class TriviaTestCase(unittest.TestCase):
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
+    print("Running test cases...")
     unittest.main()
