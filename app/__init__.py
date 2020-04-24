aeed4b2e20c56c59bffb8a8ab273257f247178f2
adam-ubuntu <gy.adam90@gmail.com>
2020-04-24 14:05:07 -0700

    Reset auth requirement for /questions

--- app/__init__.py
+++ app/__init__.py
@@ -27,8 +27,8 @@ def create_app(test_config=None):
         return response
     
     @app.route('/questions', methods=['GET'])
-    # @requires_auth('get:questions')
-    def get_questions():
+    @requires_auth('get:questions')
+    def get_questions(jwt):
         error = False
         page = request.args.get('page', 1, type=int)
         start = (page - 1) * 10
