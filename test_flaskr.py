f8a261e8d848491fd3b952b26a4bb576d1c29ebf
adam-ubuntu <gy.adam90@gmail.com>
2020-04-24 17:19:15 -0700

    Update test cases

--- test_flaskr.py
+++ test_flaskr.py
@@ -16,6 +16,8 @@ class TriviaTestCase(unittest.TestCase):
         self.app = app.create_app()
         self.client = self.app.test_client
         self.database_path = os.getenv("DATABASE_URL")
+        self.quizmaster_token = "Bearer " + os.getenv("QUIZMASTER_TOKEN")
+        self.admin_token = "Bearer " + os.getenv("ADMIN_TOKEN")
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -32,37 +34,71 @@ class TriviaTestCase(unittest.TestCase):
             self.client().delete('/questions/' + str(test_entry.id))
 
     def test_get_paginated_questions(self):
+        # test public (unauthenticated) access
         res = self.client().get('/questions')
         data = json.loads(res.data)
+        self.assertEqual(res.status_code, 401)
 
+        # test quizmaster access
+        res = self.client().get('/questions', headers={'Authorization': self.quizmaster_token})
+        data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
-        self.assertTrue(data['total_questions'])
-        self.assertTrue(data['categories'])
+        self.assertTrue(data['questions'])
+
+        # test admin access
+        res = self.client().get('/questions', headers={'Authorization': self.admin_token})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['questions'])
 
     def test_get_categories(self):
+        # test public (unauthenticated) access
         res = self.client().get('/categories')
         data = json.loads(res.data)
-
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['categories']['1'], 'Science')
 
-    def test_post_question(self):
-        add_res = self.client().get('/add')
-        self.assertEqual(add_res.status_code, 200)
+        # test unallowed method
+        mock_data = json.dumps({'category': 'Engineering'})
+        res = self.client().post('/categories', data=mock_data,
+                                      content_type='application/json')
+        self.assertEqual(res.status_code, 405)
 
-        num_questions_before = Question.query.count()
-        # Add test question, which is deleted by the teardown() function
+    def test_post_new_question(self):
+        # Test question, which is deleted by the teardown() function
         mock_data = json.dumps({
             'question': 'Test question',
             'answer': 'Test answer',
             'category': 1,
             'difficulty': 1
         })
-        post_res = self.client().post('/questions', data=mock_data,
+        # test public (unauthenticated) access
+        num_questions_before = Question.query.count()
+        res = self.client().post('/questions', data=mock_data,
                                       content_type='application/json')
         num_questions_after = Question.query.count()
 
-        self.assertEqual(post_res.status_code, 200)
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(num_questions_before, num_questions_after)
+
+        # test quizmaster access
+        num_questions_before = Question.query.count()
+        res = self.client().post('/questions', data=mock_data,
+                                      content_type='application/json',
+                                      headers={'Authorization': self.quizmaster_token})
+        num_questions_after = Question.query.count()
+
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(num_questions_before, num_questions_after)
+
+        # test admin access
+        num_questions_before = Question.query.count()
+        res = self.client().post('/questions', data=mock_data,
+                                      content_type='application/json',
+                                      headers={'Authorization': self.admin_token})
+        num_questions_after = Question.query.count()
+
+        self.assertEqual(res.status_code, 200)
         self.assertEqual(num_questions_before + 1, num_questions_after)
 
     def test_search(self):
@@ -70,25 +106,51 @@ class TriviaTestCase(unittest.TestCase):
         test_question = Question(
             question='Test question', answer='Test answer', category=1, difficulty=1)
         test_question.insert()
-
-        # Add search term
+        # Search term
         mock_data = json.dumps({
             'searchTerm': 'Test'
         })
+
+        # test public (unauthenticated) access
         res = self.client().post('/questions', data=mock_data,
                                  content_type='application/json')
         data = json.loads(res.data)
+        self.assertEqual(res.status_code, 401)
+
+        # test quizmaster access
+        res = self.client().post('/questions', data=mock_data,
+                                 content_type='application/json',
+                                 headers={'Authorization': self.quizmaster_token})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 401)
+
+        # test admin access
+        res = self.client().post('/questions', data=mock_data,
+                                 content_type='application/json',
+                                 headers={'Authorization': self.admin_token})
+        data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertTrue(data['questions'])
+        
 
     def test_categorized_questions(self):
+        # test public (unauthenticated) access
         res = self.client().get('/categories/1/questions')
-        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.status_code, 401)
 
+        # test quizmaster access
+        res = self.client().get('/categories/1/questions',
+                                headers={'Authorization': self.quizmaster_token})
         data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
         self.assertTrue(data['questions'])
-        self.assertEqual(data['totalQuestions'], len(data['questions']))
-        self.assertTrue(data['currentCategory'], 1)
+
+        # test admin access
+        res = self.client().get('/categories/1/questions',
+                                headers={'Authorization': self.admin_token})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['questions'])
 
 
 # Make the tests conveniently executable
